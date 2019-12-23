cbe3d5c2ef58ccbb7719ccca66dbf79147a5a34f
pratikn1998 <pratiknallamotu@gmail.com>
2019-12-22 19:00:47 -0500

    inital parser class

---
+++ Parser.h
@@ -0,0 +1,19 @@
+//
+// Created by Pratik Nallamotu on 12/22/19.
+//
+
+#ifndef FYE_PARSER_H
+#define FYE_PARSER_H
+
+
+#include <vector>
+#include <string>
+#include "Token.h"
+
+
+class Parser {
+
+};
+
+
+#endif //FYE_PARSER_H
